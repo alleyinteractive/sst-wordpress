2b040e846efaafb7ed1c5501d3e683e56cb9f96e
Matthew Boynes <git@mattboynes.com>
2019-04-25 11:16:43 -0400

    Initial commit of scaffolded files

---
+++ README.md
@@ -0,0 +1,4 @@
+SST WordPress Plugin
+====================
+
+This plugin provides functionality required by SST to migrate content into WordPress.
---
+++ sst.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     SST
+ * Plugin URI:      https://github.com/alleyinteractive/sst-wordpress/
+ * Description:     SST WordPress Adapter
+ * Author:          Matthew Boynes
+ * Author URI:      https://www.alley.co/
+ * Text Domain:     sst
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package SST
+ */
+
+namespace SST;
