05509165d735185123ca5113df88655c0feeb22c
Matthew Boynes <git@mattboynes.com>
2019-05-17 16:10:24 -0400

    Test setting image size via sst’s template dsl

--- tests/test-rest-api.php
+++ tests/test-rest-api.php
@@ -746,6 +746,54 @@ EOT;
 		);
 	}
 
+	public function test_replace_image_ref_with_size() {
+		$sst_source_id = 'image-33445';
+		$url           = 'https://wpthemetestdata.files.wordpress.com/2008/06/canola2.jpg';
+
+		$content = <<<EOT
+
+<!-- wp:image {"id":{{ {$sst_source_id} | to id }}} -->
+<figure class="wp-block-image"><img src="{{ {$sst_source_id} | to url | size thumbnail }}" alt="" class="wp-image-{{ {$sst_source_id} | to id }}" /></figure>
+<!-- /wp:image -->
+
+EOT;
+
+		$replaced_content = <<<EOT
+
+<!-- wp:image {"id":%1\$d} -->
+<figure class="wp-block-image"><img src="%2\$s" alt="" class="wp-image-%1\$d" /></figure>
+<!-- /wp:image -->
+
+EOT;
+
+		$data = $this->create_post_request_with_defaults(
+			[
+				'content'    => $content,
+				'references' => [
+					[
+						'type'          => 'post',
+						'subtype'       => 'attachment',
+						'sst_source_id' => $sst_source_id,
+						'args'    => compact( 'url' ),
+					],
+				],
+			]
+		);
+
+		$this->assertFalse( empty( $data['posts'][0]['post_id'] ) );
+		$this->assertFalse( empty( $data['posts'][1]['post_id'] ) );
+
+		$post     = get_post( $data['posts'][0]['post_id'] );
+		$image_id = $data['posts'][1]['post_id'];
+		$new_url  = wp_get_attachment_image_url( $image_id, 'thumbnail' );
+
+		// Confirm that the image data was replaced into the content.
+		$this->assertSame(
+			sprintf( $replaced_content, $image_id, $new_url ),
+			$post->post_content
+		);
+	}
+
 	public function test_replace_file_ref_in_content() {
 		$sst_source_id = 'file:667788';
 		$url           = 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
